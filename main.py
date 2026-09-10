e62d648326f51c109325165f4b19a6c245dc0ea9
mansurrovv10 <mansurovtilek99@gmail.com>
2026-09-11 02:24:38 +0600

    include courses, exercises, lessons, languages, gamification, progress, reviews routers

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from backend.api.users import user_router
 from backend.api.friends import friend_router
 from backend.api.chats import router as chats_router
 from backend.api.chat_ws import websocket_router
+from backend.api.courses import router as courses_router
+from backend.api.exercises import router as exercises_router
+from backend.api.lessons import router as lessons_router
+from backend.api.languages import router as languages_router
+from backend.api.gamification import router as gamification_router
+from backend.api.progress import router as progress_router
+from backend.api.reviews import router as reviews_router
 
 
 duolingo = FastAPI(title="Mini Duolingo")
@@ -15,6 +22,13 @@ duolingo.include_router(websocket_router)
 duolingo.include_router(auth_router)
 duolingo.include_router(user_router)
 duolingo.include_router(friend_router)
+duolingo.include_router(courses_router)
+duolingo.include_router(exercises_router)
+duolingo.include_router(lessons_router)
+duolingo.include_router(languages_router)
+duolingo.include_router(gamification_router)
+duolingo.include_router(progress_router)
+duolingo.include_router(reviews_router)
 
 
 if __name__ == '__main__':
